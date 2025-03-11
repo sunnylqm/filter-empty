841abdd94e217e18253bf640ba0fa338919d7e2f
Sunny Luo <sunnylqm@gmail.com>
2025-03-11 22:12:13 +0800

    Update isObject.ts

--- src/lib/isObject.ts
+++ src/lib/isObject.ts
@@ -1,6 +1,6 @@
 function isObject(value: any): boolean {
   if (value === null) return false;
-  return typeof value === 'object';
+  return typeof value === 'object' && value instanceof Date === false;
 }
 
 export default isObject;
